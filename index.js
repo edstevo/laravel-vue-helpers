98018f4a52163ff522f53414ff875cad5a51e5cc
edstevo <edward.stephenson@me.com>
2017-01-06 22:43:31 +0000

    route parser

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ed on 06/01/2017.
+ */
+
+var LaravelRouteParser  = require('./route-parser/main');
+
+module.exports = {
+
+    LaravelRouteParser: LaravelRouteParser
+};
---
+++ route-parser/main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Ed on 02/01/2017.
+ */
+
+module.exports = (function () {
+
+    function install (Vue, options) {
+
+        this.routes = options.routes;
+
+        Vue.prototype.parseRoute = function(route, params) {
+            var href    = this.routes[route];
+
+            for (var prop in params) {
+                href    = href.replace("{" + prop + "}", params[prop]);
+            }
+
+            return href;
+        }
+    }
+
+    if (typeof exports == "object") {
+        module.exports = install
+    } else if (typeof define == "function" && define.amd) {
+        define([], function(){ return install })
+    } else if (window.Vue) {
+        Vue.use(install)
+    }
+
+})();
