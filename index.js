6d592a2cbc95264c7e35cf4dcface57aabff586e
edstevo <edward.stephenson@me.com>
2017-01-06 22:56:27 +0000

    -m "uploading"

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
  * Created by Ed on 06/01/2017.
  */
 
-var LaravelRouteParser  = require('./route-parser/main');
-
 module.exports = {
 
-    LaravelRouteParser: LaravelRouteParser
+    LaravelRouteParser: require('./route-parser/main')
 };
